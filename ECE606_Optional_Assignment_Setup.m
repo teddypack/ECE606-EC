fd9c3e403a0a8804aa0fe3a526d9fa60b1e526f5
teddypack <40552233+teddypack@users.noreply.github.com>
2020-12-03 14:32:49 -0500

    Restructure of files

--- ECE606_Optional_Assignment_Setup.m
+++ ECE606_Optional_Assignment_Setup.m
@@ -1,3 +1,4 @@
+%**************************************************************************
 % This script sets parameters and performs intial calculations for the 3 
 % noted devices:
 %   1) Si BJT
@@ -8,8 +9,32 @@
 %
 % Emitter and Collector remain Si for the HTB devices
 %
+%*************************Team Members*************************************
+% Heather Hoftsee
+% hhofstee@purdue.edu
+% Nicholas Morrissey 
+% nmorriss@purdue.edu
+% Ted Packowski
+% tpackows@purdue.edu
+%**************************************************************************
+%
+%******************************Sources*************************************
+% Equations
+% https://www.eit.lth.se/fileadmin/eit/courses/fff115/Lectures/F5.pdf
+% Material Properties
+% http://www.ioffe.ru/SVA/NSM/Semicond/SiGe/
+% http://www.ioffe.ru/SVA/NSM/Semicond/SiGe/bandstr.html#carrier%20concentration 
+% http://www.virginiasemi.com/pdf/generalproperties%20Si62002.DOC
+%**************************************************************************
+%
 %********************************Internal**********************************
-% added comment
+% Ne = Doping concentration in the emitter 
+% Nb = Doping concentration in the base
+% Nc = Doping concentration in the collector
+% We = Total width of the emitter in the BJT/HBT
+% Wb = Total width of the base in the BJT/HBT
+% Wc = Total width of the collector in the BJT/HBT
+% T  = Temperature in Kelvin
 %**************************************************************************
 %
 %**********************Global Material Properties**************************
@@ -25,6 +50,7 @@ T = 300;   %temperature in Kelvin
 %**********************Device 1 Material Properties************************
 
 %intrinsic carrier concentration
+ni1= ; %intrinsics carrier concentration in device 1
 %mobility
 %diffusion coefficient
 %dielectric constants
@@ -52,22 +78,5 @@ T = 300;   %temperature in Kelvin
 %dielectric constants
 %electron affinity
 %valence band offsets
-%**************************************************************************
 %
-%*************************Team Members*************************************
-% Heather Hoftsee
-% hhofstee@purdue.edu
-% Nicholas Morrissey 
-% nmorriss@purdue.edu
-% Ted Packowski
-% tpackows@purdue.edu
-%**************************************************************************
-
-%******************************Sources*************************************
-% Equations
-% https://www.eit.lth.se/fileadmin/eit/courses/fff115/Lectures/F5.pdf
-% Material Properties
-% http://www.ioffe.ru/SVA/NSM/Semicond/SiGe/
-% http://www.ioffe.ru/SVA/NSM/Semicond/SiGe/bandstr.html#carrier%20concentration 
-% http://www.virginiasemi.com/pdf/generalproperties%20Si62002.DOC
-%**************************************************************************
+%********************************End Code**********************************
---
+++ ECE606_Optional_Assignment_Study.m
@@ -0,0 +1,25 @@
+%**************************************************************************
+% This script initializes parameters, runs study and plots results for 
+% ECE 606 Optional Assignment.
+%**************************************************************************
+%
+%*************************Team Members*************************************
+% Heather Hoftsee
+% hhofstee@purdue.edu
+% Nicholas Morrissey 
+% nmorriss@purdue.edu
+% Ted Packowski
+% tpackows@purdue.edu
+%**************************************************************************
+%
+%**************************Initialize Parameters***************************
+ECE606_Optional_Assignment_Setup;
+%
+%*******************************Run Study**********************************
+%
+%Calculations go here
+%
+%******************************Plot Results********************************
+%ECE606_Optional_Assignment_Plot('Plot Name',Structure,Fig Number);
+%
+%********************************End Code**********************************
