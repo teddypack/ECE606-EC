fd7496d4771014798c5105714edd7c601dc431bc
teddypack <40552233+teddypack@users.noreply.github.com>
2020-12-05 15:21:04 -0500

    update

--- ECE606_Optional_Assignment_Study.m
+++ ECE606_Optional_Assignment_Study.m
@@ -44,7 +44,7 @@ xnbc1 = sqrt((VbiBC1*2*KSi*epsilon0*Nb)/(q*Nc*(Nc+Nb))); %width of CB1 depletion
 W1=Wb-xnbe1-xnbc1; %electrical base width in device 1 (in um)
 %
 %***********************Abrupt junction HBT Calcs**************************
-
+%disp(x0);
 %*****************Minority Carrier Mobility in the Base********************
 for i=1:6
     munb2(i)=7333.333*(1-x0(i))-5466.667; %for 0<x<0.3
@@ -66,19 +66,15 @@ end
 %**********************SiGe Electron Affinity******************************
 ChiSiGe = 4 + 0.05 * x0;
 
-DeltaEv = EgSi - EgSiGe - (ChiSiGe - ChiSi);
-
-disp(x0);
+DeltaEv = abs(EgSi - EgSiGe) - abs(ChiSiGe - ChiSi);
 beta2 = ((Dnb2*We*Ne)/(Dpe*Wb*Nb)).*exp(DeltaEv/kBT); % common emitter DC gain
-alpha2 = beta2 / (beta2 +1); %common base DC gain
-%disp(beta2);
-%disp(alpha2);
+alpha2 = beta2 ./ (beta2 +1); %common base DC gain
 % VbiBE2 = %built in potential across BE2 junction
 % VbiBC2 = %built in potential across BC2 junction
 % xnbe2 = %width of EB2 depletion region
 % xnbc2 = %width of CB2 depletion region
 % W2 = Wb-xnbe2-xnbc2; %electrical base width in device 2 (in um)
-% 
+ 
 %***********************Graded junction HBT Calcs**************************
 for i=1:2
      nib3(i)=sqrt((5.3e15*T^1.5)*(4.82e15*4*(0.81-0.47*(1-x0(i)))*T^1.5)).*exp(-EgSiGe(i)/(2*kBT));
@@ -87,9 +83,7 @@ for i=3:13
      nib3(i)=sqrt((2e15*T^1.5)*(4.82e15*4*(0.81-0.47*(1-x0(i)))*T^1.5)).*exp(-EgSiGe(i)/(2*kBT));
 end
 beta3 = (((nib3.^2).*Dnb2)*We*Ne)/(Dpe*Wb*Nb*niSi^2); % common emitter DC gain
-disp(beta3);
 alpha3 = beta3 ./ (beta3 +1); %common base DC gain
-disp(alpha3);
 % VbiBE3 = %built in potential across BE3 junction
 % VbiBC3 = %built in potential across BC3 junction
 % xnbe3 = %width of EB3 depletion region
@@ -100,45 +94,54 @@ disp(alpha3);
 %******************************Plot Results********************************
 
 %***************************Plot Table of info*****************************
-DataTable = strings(14,4);
+DataTable = strings(29,4);
 DataTable(1,:) = ["BJT",beta1,alpha1,W1];
-for i=2:14
-    DataTable(i,1)=x0(i-1);
+DataTable(2,:) = ["=====","Device 2","=====","=========="];
+DataTable(16,:) = ["=====","Device 3","=====","=========="];
+for i=3:15
+    DataTable(i,1)=x0(i-2);
+    DataTable(i,2)=sprintf('%1.5g',beta2(i-2));
+    DataTable(i,3)=sprintf('%1.5g',alpha2(i-2));
+    DataTable(i+14,1)=x0(i-2);
+    DataTable(i+14,2)=sprintf('%1.5g',beta3(i-2));
+    DataTable(i+14,3)=sprintf('%1.5g',alpha3(i-2));
 end
 %disp(DataTable);
-fig = uifigure('Name','Table Results');
+fig = uifigure;
+fig.Name = 'Table of Results';
+fig.Position = [500 300 459 677 ];
 uit = uitable(fig,'Data',DataTable);
 uit.ColumnName={'x0','βDC','αDC','Electrical Base Width'};%);
-uit.ColumnWidth={64,64,64,164};
-uit.Position=[10 10 398 337];
+uit.ColumnWidth={64,114,64,164};
+uit.Position = [5 5 449 667];
 
 %*********************Plot Common Emitter DC Gain**************************
-figure(2)
-plot(beta1);
-yline(beta1,'-',sprintf('BJT=%0.4f',beta1));
-axis([0 1.1 0 100]);
-xlabel('x0');
-ylabel('Gain');
-title(['Common Emitter DC Gain']);
-grid on;
-
-%***********************Plot Common Base DC Gain***************************
-figure(3)
-plot(alpha1);
-yline(alpha1,'-',sprintf('BJT=%0.4f',alpha1));
-axis([0 1.1 0 1.1]);
-xlabel('x0');
-ylabel('Gain');
-title(['Common Base DC Gain']);
-grid on;
-
-%***********************Plot Electrical Base Width*************************
-figure(4)
-plot(W1);
-yline(W1,'-',sprintf('BJT=%0.4f',W1));
-axis([0 1.1 0 1.1]);
-xlabel('x0');
-ylabel('Width (µm)');
-title(['Electrical Base Width']);
-grid on;
-%********************************End Code**********************************
+% figure(2)
+% plot(beta1);
+% yline(beta1,'-',sprintf('BJT=%0.4f',beta1));
+% axis([0 1.1 0 100]);
+% xlabel('x0');
+% ylabel('Gain');
+% title(['Common Emitter DC Gain']);
+% grid on;
+% 
+% %***********************Plot Common Base DC Gain***************************
+% figure(3)
+% plot(alpha1);
+% yline(alpha1,'-',sprintf('BJT=%0.4f',alpha1));
+% axis([0 1.1 0 1.1]);
+% xlabel('x0');
+% ylabel('Gain');
+% title(['Common Base DC Gain']);
+% grid on;
+% 
+% %***********************Plot Electrical Base Width*************************
+% figure(4)
+% plot(W1);
+% yline(W1,'-',sprintf('BJT=%0.4f',W1));
+% axis([0 1.1 0 1.1]);
+% xlabel('x0');
+% ylabel('Width (µm)');
+% title(['Electrical Base Width']);
+% grid on;
+% %********************************End Code**********************************
